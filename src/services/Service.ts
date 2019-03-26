e5de4c02eb2f18007565ec765e62d90bcbd84c24
Sander Vispoel <sander.vispoel@labela.nl>
2019-03-26 20:51:30 +0100

    Disable database

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,12 +1,12 @@
 import { EntitySchema } from 'typeorm';
-import Database from 'database';
+// import Database from 'database';
 
 export abstract class Service<E> {
   constructor(
     protected entity: string | (() => {}) | (new () => E) | EntitySchema<E>
   ) {}
 
-  protected get repo() {
-    return Database.connection.getRepository<E>(this.entity);
-  }
+  // protected get repo() {
+    // return Database.connection.getRepository<E>(this.entity);
+  // }
 }
