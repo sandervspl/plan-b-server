0a7b52f359be670f9cb3e1dd8e9bd272d7002627
Sander Vispoel <sander.vispoel@labela.nl>
2019-05-13 23:10:24 +0200

    Remove session user type overwrite

--- src/types/general.ts
+++ src/types/general.ts
@@ -1,18 +1,6 @@
 import * as i from 'types'; // eslint-disable-line
 import { Repository } from 'typeorm';
 
-declare global {
-  // eslint-disable-next-line
-  namespace Express {
-    interface Request {
-      user?: i.UserData & {
-        authLevel: number;
-        avatarUrl: string;
-      };
-    }
-  }
-}
-
 export interface Repositories {
   [index: string]: Repository<any>; // eslint-disable-line @typescript-eslint/no-explicit-any
 }
