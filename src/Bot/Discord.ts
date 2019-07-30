19d7adc470457370e9616daa044cf4c3e179df32
Sander Vispoel <sander.vispoel@labela.nl>
2019-07-30 09:17:02 +0200

    Remove bot messages code

--- src/Bot/Discord.ts
+++ src/Bot/Discord.ts
@@ -1,6 +1,5 @@
 import Discord from 'discord.js';
 import secretConfig from 'config/secret';
-import messages from './messages';
 
 class DiscordBot {
   public readonly client = new Discord.Client();
@@ -11,12 +10,6 @@ class DiscordBot {
     this.client.on('ready', () => {
       console.info('Discord bot activated.');
     });
-
-    this.initMessageServices();
-  }
-
-  private initMessageServices = () => {
-    messages.forEach((message) => new message(this.client));
   }
 }
 
