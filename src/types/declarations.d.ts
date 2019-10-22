9c308a5858cf3e12adfcef79d1df7c011e8819fc
Sander Vispoel <sander.vispoel@labela.nl>
2019-10-22 11:52:09 +0200

    Fix type declarations for node env

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,3 +1,7 @@
-export interface ProcessEnv {
-  APP_ENV: 'development' | 'test' | 'acceptation' | 'production';
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    APP_ENV: 'development' | 'test' | 'acceptation' | 'production';
+    PUBLIC_URL: string;
+    NODE_ENV: 'development' | 'production';
+  }
 }
