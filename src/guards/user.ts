928a4bac2a3e6d7e3d45204e4d5473c0c134f37a
Sander Vispoel <sander.vispoel@labela.nl>
2019-11-04 17:07:11 +0100

    Disable user guard

--- src/guards/user.ts
+++ src/guards/user.ts
@@ -7,9 +7,10 @@ export class UserGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const req = context.switchToHttp().getRequest<Request>();
 
-    if (!req.session || !req.user) {
-      throw new UnauthorizedException();
-    }
+    /** @TODO always false */
+    // if (!req.session || !req.user) {
+    //   throw new UnauthorizedException();
+    // }
 
     return true;
   }
